5c16756703e074d97503df0d2290ffd275ee462e
seongmin <seongmin1117@gmail.com>
2025-02-03 12:37:44 +0900

    Initial commit

---
+++ src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sparta.myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
