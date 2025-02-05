0108a78029a20d1d3ea0fa76e5654dce7db26e9f
seongmin <seongmin1117@gmail.com>
2025-02-06 07:25:30 +0900

    refactor: 클라이언트-서버 요청 응답 형식 통일

--- src/main/java/com/sparta/myselectshop/product/presentation/ProductController.java
+++ src/main/java/com/sparta/myselectshop/product/presentation/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,9 +56,9 @@ public class ProductController {
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
-  @PutMapping()
+  @PutMapping("{id}")
   public ResponseEntity<Void> updateMyPrice(
-      @Valid @RequestParam Long id, @RequestBody MyPriceUpdateRequest request) {
+      @PathVariable Long id, @RequestBody MyPriceUpdateRequest request) {
     productService.updateMyPrice(id, request);
     return ResponseEntity.ok().build();
   }
