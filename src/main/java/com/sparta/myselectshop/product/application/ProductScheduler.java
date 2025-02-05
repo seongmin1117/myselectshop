2ea7d146b03e12cc6ba8b8537a29e24bc58c59ba
seongmin <seongmin1117@gmail.com>
2025-02-05 12:03:12 +0900

    feat: 상품 페이징 및 정렬 구현

--- src/main/java/com/sparta/myselectshop/product/application/ProductScheduler.java
+++ src/main/java/com/sparta/myselectshop/product/application/ProductScheduler.java
@@ -3,8 +3,7 @@ package com.sparta.myselectshop.product.application;
 import com.sparta.myselectshop.naver.application.NaverService;
 import com.sparta.myselectshop.naver.application.dto.NaverSearchItem;
 import com.sparta.myselectshop.naver.application.dto.NaverSearchItems;
-import com.sparta.myselectshop.product.application.dto.ProductListResponse;
-import com.sparta.myselectshop.product.application.dto.ProductResponse;
+import com.sparta.myselectshop.product.domain.Product;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
@@ -22,24 +21,25 @@ public class ProductScheduler {
   @Scheduled(cron = "0 0 1 * * *")
   public void updatePrice() throws InterruptedException {
     log.info("상품 최저가 업데이트 실행");
-    ProductListResponse response = productService.getProducts();
-    List<ProductResponse> products = response.products();
-
-    for (ProductResponse product : products) {
+    List<Product> products = productService.findAll();
+    for (Product product : products) {
       // 1초에 한 상품씩 조회 (Naver API 제한)
       TimeUnit.SECONDS.sleep(1);
       updateBySearch(product);
     }
   }
 
-  private void updateBySearch(ProductResponse product) {
-    NaverSearchItems searchResults = naverService.searchItems(product.title());
+  private void updateBySearch(Product product) {
+    NaverSearchItems searchResults = naverService.searchItems(product.getTitle());
     if (searchResults.items().isEmpty()) {
-      log.warn("[가격 미갱신] '{}' 상품의 네이버 검색 결과 없음", product.title());
+      log.warn("[가격 미갱신] '{}' 상품의 네이버 검색 결과 없음", product.getTitle());
     }
     NaverSearchItem bestMatchItem = searchResults.items().get(0);
-    productService.updateBySearch(product.productId(), bestMatchItem.lprice());
+    productService.updateBySearch(product.getId(), bestMatchItem.lprice());
     log.info(
-        "[가격 업데이트] 상품 '{}': {}원 → {}원", product.title(), product.lprice(), bestMatchItem.lprice());
+        "[가격 업데이트] 상품 '{}': {}원 → {}원",
+        product.getTitle(),
+        product.getLprice(),
+        bestMatchItem.lprice());
   }
 }
